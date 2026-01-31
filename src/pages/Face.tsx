470e0a5517c37a7abbeb6b5a673ed701a1f61480
momotabetai0110 <momotabetai0110@gmail.com>
2026-01-31 18:39:02 +0900

    ルーティング追加

---
+++ src/pages/Face.tsx
@@ -0,0 +1,13 @@
+import { Link } from "react-router-dom";
+
+export default function Face() {
+  return (
+    <div>
+      <h2>Face</h2>
+
+      <Link to="/">
+        <button>Homeへ</button>
+      </Link>
+    </div>
+  );
+  }
